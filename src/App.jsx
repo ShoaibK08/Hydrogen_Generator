753f87d12f20efa2bf5d08ad9bde993f7e66c05a
Deepak-96adt <deepakupadhyay.adt@gmail.com>
2024-12-23 20:27:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import Header from './components/Header';
+// import Header2 from './components/Header2';
+// import Theory from './components/Theory';
+import HydrogenLearning from './pages/HydrogenLearning';
+
+
+const App = () => {
+  return (
+    <div className="font-['Outfit'] text-[#052524]">
+      {/* <Theory /> */}
+      <HydrogenLearning />
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/data/LearningData.js
@@ -0,0 +1,115 @@
+export const learningData = [
+    {
+      title: 'Hydrogen Basics',
+      content: [
+        { text: 'Introduction to Hydrogen', url: 'javascript:void(0)' },
+        { text: 'Hydrogen as Fuel', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Categories of Hydrogen based on production',
+      content: [
+        { text: 'Grey Hydrogen', url: 'javascript:void(0)' },
+        { text: 'Blue Hydrogen', url: 'javascript:void(0)' },
+        { text: 'Green Hydrogen', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Hydrogen Safety', 
+      content: [
+        { text: 'Flammability and Explosivity', url: 'javascript:void(0)' },
+        { text: 'Leakage and Detection', url: 'javascript:void(0)' },
+        { text: 'Material Compatibility', url: 'javascript:void(0)' },
+        { text: 'Handling and Transport', url: 'javascript:void(0)' },
+        { text: 'Education and Awareness', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Hydrogen Generation Methodologies',
+      content: [
+        { text: 'Steam Methane Reforming', url: 'javascript:void(0)' },
+        { text: 'Electrolysis of Water', url: 'javascript:void(0)' },
+        { text: 'Methanol Reforming', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Hydrogen Generation through Electrolysis',
+      content: [
+        { text: 'Alkaline Electrolysis', url: 'javascript:void(0)' },
+        { text: 'PEM (Proton Exchange Membrane) Electrolysis', url: './PEM-electrolysis.html' }
+      ]
+    },
+    {
+      title: 'Hydrogen Storage',
+      content: [
+        { text: 'Geological Hydrogen Storage', url: 'javascript:void(0)' },
+        { text: 'Compressed Hydrogen', url: 'javascript:void(0)' },
+        { text: 'Liquid Hydrogen', url: 'javascript:void(0)' },
+        { text: 'Material based Hydrogen Storage', url: 'javascript:void(0)' },
+        { text: 'Hydrogen Storage in Ammonia', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Hydrogen Distribution',
+      content: [
+        { text: 'Hydrogen Pipelines', url: 'javascript:void(0)' },
+        { text: 'Hydrogen Tankers', url: 'javascript:void(0)' },
+        { text: 'Hydrogen Refueling Stations', url: 'javascript:void(0)' },
+        { text: 'Ammonia as a Hydrogen Carrier', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Fuel Cell Fundamentals',
+      content: [
+        { text: 'Introduction to Fuel Cell', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Categories of Fuel Cell',
+      content: [
+        { text: 'Alkaline Fuel Cell', url: 'javascript:void(0)' },
+        { text: 'PEM (Proton Exchange Membrane) Fuel Cell', url: 'javascript:void(0)' },
+        { text: 'Microbial Fuel Cell', url: 'javascript:void(0)' },
+        { text: 'Phosphoric Acid Fuel Cells (PAFC)', url: 'javascript:void(0)' },
+        { text: 'Molten Carbonate Fuel Cells (MCFC)', url: 'javascript:void(0)' },
+        { text: 'Solid Oxide Fuel Cells (SOFC)', url: 'javascript:void(0)' },
+        { text: 'Direct Methanol Fuel Cells (DMFC)', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Advanced Hydrogen Fuel Cell Experiments',
+      content: [
+        { text: 'Test the fuel cell at different load condition', url: 'javascript:void(0)' },
+        { text: 'Study of IV characteristics of fuel cell', url: 'javascript:void(0)' },
+        { text: 'Study of the Measurement of Fuel Cell Efficiency', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Hydrogen industrial Application',
+      content: [
+        { text: 'Ammonia Production', url: 'javascript:void(0)' },
+        { text: 'Methanol Production', url: 'javascript:void(0)' },
+        { text: 'Steelmaking', url: 'javascript:void(0)' },
+        { text: 'Concrete Production', url: 'javascript:void(0)' },
+        { text: 'Oil Refining', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Hydrogen Combustion Engine',
+      content: [
+        { text: '', url: 'javascript:void(0)' },
+        { text: '', url: 'javascript:void(0)' }
+      ]
+    },
+    {
+      title: 'Hydrogen Mobility and Transportation Applications',
+      content: [
+        { text: 'Fuel Cell Buses', url: 'javascript:void(0)' },
+        { text: 'Material Handling', url: 'javascript:void(0)' },
+        { text: 'Mining trucks', url: 'javascript:void(0)' },
+        { text: 'Aviation', url: 'javascript:void(0)' },
+        { text: 'Rail Transportation', url: 'javascript:void(0)' },
+        { text: 'Marine Transportation', url: 'javascript:void(0)' }
+      ]
+    }
+  ]
