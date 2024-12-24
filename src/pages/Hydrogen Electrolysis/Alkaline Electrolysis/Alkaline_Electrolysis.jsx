647dd7e2ee332af59212ae51430df904671aa822
Deepak-96adt <deepakupadhyay.adt@gmail.com>
2024-12-24 16:14:34 +0530

    added new header and pages

--- src/pages/Hydrogen Electrolysis/Alkaline Electrolysis/Alkaline_Electrolysis.jsx
+++ src/pages/Hydrogen Electrolysis/Alkaline Electrolysis/Alkaline_Electrolysis.jsx
@@ -1,41 +1,38 @@
+import React from 'react';
 import back from '../../../assets/icon5.png';
-import PEM_img from '../../../assets/PEM_img.png';
+import Alkaline_img from '../../../assets/Alkaline_img.jpg';
 import Header2 from '../../../components/Header2.jsx';
-import { useNavigate } from 'react-router-dom';
 
-const PEM_Electrolysis_Theory = () => {
-  const navigate = useNavigate();
+const Alkaline_Electrolysis = () => {
   return (
     <div className="bg-[#f5f5f5] min-h-screen">
       <Header2 />
       <div className="w-[95%] max-w-[90%] space-y-[30px] mx-auto py-[30px]">
         <div className="flex items-center gap-3.5">
-          <button
-            onClick={() => navigate(-1)}
+          <a 
+            href="./index.html" 
             className="bg-white rounded-full size-[30px] flex items-center justify-center border-[0.5px] border-[#05252433]"
           >
             <img src={back} alt="back" className="" />
-          </button>
+          </a>
           <h3 className="text-[26px] line-height-[32px] font-medium">
-            PEM (Proton Exchange Membrane) Electrolysis
+          Alkaline electrolysis (AEL) 
           </h3>
         </div>
-
+        
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-10 gap-y-5">
-          <img
-            src={PEM_img}
-            alt="PEM Electrolysis"
-            className="mx-auto shadow-lg rounded-2xl"
+          <img 
+            src={Alkaline_img}
+            alt="PEM Electrolysis" 
+            className="mx-auto shadow-lg rounded-2xl" 
           />
           <div className="flex-col flex items-start space-y-3.5 lg:space-y-[26px] justify-center">
             <h3 className="text-[22px] md:text-3xl lg:text-[32px] line-height-[32px] font-semibold">
               Overview
             </h3>
             <p className="text-sm lg:text-base xl:text-lg line-height-[26px] font-normal">
-              Proton Exchange Membrane (PEM) electrolysis is a method for producing hydrogen by splitting water into hydrogen and oxygen using electricity. In this process, water is passed through a proton exchange membrane, which acts as an electrolyte and allows only protons (hydrogen ions) to pass through. When an electric current is applied, water molecules are split at the electrodes; hydrogen gas forms at the cathode (negative electrode), while oxygen gas forms at the anode (positive electrode). PEM electrolysis is known for its high efficiency and compact design, making it suitable for a variety of applications, including fuel cells and clean energy systems.
-            </p>
-            <button
-
+            Alkaline electrolysis (AEL) is a process used to produce hydrogen gas by splitting water into hydrogen and oxygen using electricity. In AEL, water is mixed with an alkaline solution, usually potassium hydroxide (KOH) or sodium hydroxide (NaOH), which acts as the electrolyte. When electrical current is passed through this solution, water molecules are broken down at the electrodes: hydrogen gas forms at the cathode (negative electrode), while oxygen gas forms at the anode (positive electrode). This method is efficient and commonly used for generating hydrogen in various industrial applications. </p>
+            <button 
               className="text-xs sm:text-sm lg:text-base xl:text-lg line-height-[26px] font-normal bg-[#0177fb] text-white rounded-full px-4 sm:px-6 py-2 sm:py-2.5 active:translate-y-1 active:shadow-none transition-all duration-75 shadow-[0px_4px_0px_0px_#0155b3]"
             >
               Simulation
@@ -47,4 +44,4 @@ const PEM_Electrolysis_Theory = () => {
   );
 };
 
-export default PEM_Electrolysis_Theory;
+export default Alkaline_Electrolysis;
